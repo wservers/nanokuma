6be4f8e53f3937769af123e8d976fdfefa7dfa8b
Siwoo Jeon <me@migan.co.kr>
2026-03-20 17:48:24 +0900

    feat: read a job

--- server/modules/webserver/job.go
+++ server/modules/webserver/job.go
@@ -76,7 +76,73 @@ func JobCreate(ctx *gin.Context) {
 	ctx.JSON(201, gin.H{"ok": 1, "message": "job created!", "id": id})
 }
 
-func JobRead(ctx *gin.Context) {}
+func JobRead(ctx *gin.Context) {
+	var err error
+	var job *job.Job
+	var id, agentID string
+	var rp repo.RepoModule
+	var agent *agent.AgentData
+
+	id = ctx.Query("agent_id")
+	if id == "" {
+		ctx.JSON(400, gin.H{
+			"ok":      0,
+			"message": "\"job_id\" query must be contained",
+		})
+		return
+	}
+
+	agentID = ctx.Query("agent_id")
+	if agentID == "" {
+		ctx.JSON(400, gin.H{
+			"ok":      0,
+			"message": "\"agent_id\" query must be contained",
+		})
+		return
+	}
+
+	if repo.Repo == nil {
+		ctx.JSON(500, gin.H{
+			"ok":      0,
+			"message": "\"repo\" service not served! please contact server administrator.",
+		})
+		return
+	}
+
+	rp = *repo.Repo
+
+	agent, err = rp.GetAgent(id)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"ok":      0,
+			"message": "failed to get the agent information",
+		})
+		return
+	}
+
+	if !agent.Authorized {
+		ctx.JSON(403, gin.H{
+			"ok":      0,
+			"message": "the agent is not authorized",
+		})
+		return
+	}
+
+	job, err = rp.GetJob(id)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"ok":      0,
+			"message": "failed to get the job information",
+		})
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"ok":      1,
+		"message": "the job found",
+		"data":    job,
+	})
+}
 
 func JobQuery(ctx *gin.Context) {}
 
