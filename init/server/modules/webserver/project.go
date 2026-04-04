d5962241de4d35c6e3a3a5cc1f702e433973a186
Siwoo Jeon <me@migan.co.kr>
2026-04-04 18:20:40 +0900

    fix: get project by repo url

--- init/server/modules/webserver/project.go
+++ init/server/modules/webserver/project.go
@@ -37,55 +37,27 @@ func ProjectCreate(ctx *gin.Context) {
 
 func ProjectRead(ctx *gin.Context) {
 	var err error
-	var id string
+	var id, repoURL string
 	var rp repo.RepoModule
 	var project *project.Project
 
 	id = ctx.Query("project_id")
-	if id == "" {
-		ctx.JSON(400, gin.H{
-			"ok":      0,
-			"message": "\"project_id\" query must be contained",
-		})
-		return
-	}
-
-	rp = *repo.Repo
-
-	project, err = rp.GetProject(id)
-	if err != nil {
-		ctx.JSON(500, gin.H{
-			"ok":      0,
-			"message": "failed to get the project information.",
-		})
-		return
-	}
-
-	ctx.JSON(200, gin.H{
-		"ok":      1,
-		"message": "the project found",
-		"data":    project,
-	})
-}
-
-func ProjectReadByRepoURL(ctx *gin.Context) {
-	var err error
-	var repoURL string
-	var rp repo.RepoModule
-	var project *project.Project
-
 	repoURL = ctx.Query("repo_url")
-	if repoURL == "" {
+
+	rp = *repo.Repo
+
+	if id != "" {
+		project, err = rp.GetProject(id)
+	} else if repoURL != "" {
+		project, err = rp.GetProjectByRepoURL(repoURL)
+	} else {
 		ctx.JSON(400, gin.H{
 			"ok":      0,
-			"message": "\"repo_url\" query must be contained",
+			"message": "\"project_id\" or \"repo_url\" query must be contained",
 		})
 		return
 	}
 
-	rp = *repo.Repo
-
-	project, err = rp.GetProjectByRepoURL(repoURL)
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"ok":      0,
